4ee7191ba08c9fa298aa273c11a3daeb1224dc1f
BenL19 <g00371775@gmit.ie>
2020-02-20 14:49:42 +0000

    constructers

--- Date.java
+++ Date.java
@@ -4,30 +4,47 @@
 public class Date {
     
     // Instance Variables
-    private int day;
-    private int month;
-    private int year;
+    int day;
+    int month;
+    int year;
+    String event;
 
     // Default Constructor (No Parameters)
     public Date(){
-        //Your code here
-    }
-
-    // Parameterized constructor (3 Parameters)
-    public Date(int newDay, int newMonth, int newYear){
-        // Your code here.
+        day = 0;
+        month = 0;
+        year = 0;
+        event = "Nothing on today";
     }
     
     // Parameterized constructor (2 Parameters)
-    // Your code here.
+    public Date(int day, int month){
+        this(day);
+        this.month = month;
+    }
+    
+    // Parameterized constructor (3 Parameters)
+    public Date(int day, int month, int year){
+        this(day, month);
+        this.year = year;
+    }
+    
     
     // Parameterized constructor (1 Parameter)
-    // Your code here.
+    public Date(int day){
+        this.day = day;
+    }
+
+    // Parameterized constructor (3 Parameters)
+    public Date(int day, int month, int year, String event){
+        this(day, month, year);
+        this.event = event;
+    }
     
     // A simple print method
     public void printDate(){
         // Print the date like this: day/month/year
-        // Your code here
+        System.out.println(this.day + "/" + this.month + "/" +this.year+" "+event);
     }
 
 }
